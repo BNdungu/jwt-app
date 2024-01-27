ab39fd41b3fe8d2200187b6f5065ddd995d0897b
BNdungu <bryannganga909@gmail.com>
2024-01-27 13:52:07 +0300

    tokens

--- controllers/main.js
+++ controllers/main.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken')
 const CustomApiError = require('../errors/custom-error')
 
 const login = async (req,res) => {
@@ -5,13 +6,30 @@ const login = async (req,res) => {
     if (!username || !password) {
         throw new CustomApiError('Please provide login details', 400)
     }
+    
+    const id = new Date().getMonth()
+    const token = jwt.sign({id, username},process.env.JWT_SECRET,{expiresIn: '30d'})
 
-    res.send('Fake login/register route')
+    res.status(200).json({msg: 'user created', date: id, token})
 }
 
 const dashboard = async (req,res) => {
+    const authHeader = req.headers.authorisation
     const luckyNumber = Math.floor(Math.random()*100)
-    res.status(200).json({msg : 'Hello John doe', secret: `Here is you secret message ${luckyNumber}`})
+    if (!authHeader || !authHeader.startsWith("Bearer ")){
+        throw new CustomApiError('Invalid token', 401)
+    }
+    const token = authHeader.split(' ')[1]
+    
+
+    try {
+        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+        res.status(200).json({msg : `Hello ${decoded.username}`, secret: `Here is you secret message ${luckyNumber}`})
+    } catch (error) {
+        throw new CustomApiError('Not authorised to acces this route', 401)
+    }
+
+    
 }
 
 module.exports = {
